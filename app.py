e37b30d20c3fdba816a4066683348b508e22c49e
dinapark <skandinapark@gmail.com>
2023-01-24 17:41:12 +0100

    login problem is fixed

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -8,18 +8,17 @@ app = Flask(__name__)
 def test():
     return render_template('index.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+@app.route('/login_check', methods=["POST"])
+def login_check():
+    userId = request.form['id']
+    userPwd = request.form['pwd']
 
-@app.route('/login_confirm', methods=['POST'])
-def login_comfirm():
-    id = request.form['id']
-    pw = request.form['pw']
-    if id == 'admin' and pw == 'admin':
-        return redirect(url_for('login'))
+    if userId  == 'admin' and userPwd == 'admin123':
+        return render_template('login.html')
     else:
-        return redirect(url_for('index'))
+        return render_template('index.html')
+    
+    return print("username:", username, "password:", password)
 
 if __name__=="__main__":
     app.run(host='0.0.0.0', port='8080', debug=True)
