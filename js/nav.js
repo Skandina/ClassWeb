7c0b3b0564fc6bc0f1a35a441493fb1e5d1dd583
JheeKim <jheekim992@gmail.com>
2023-01-26 17:53:50 +0900

    Add navigation bar

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector(".toggleBtn");
+const menu = document.querySelector("#menu-list");
+const topMenu = document.querySelector("#top-menu");
+
+toggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  topMenu.classList.toggle("active");
+});
