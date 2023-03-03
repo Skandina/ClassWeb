0e8bab4e16a3df50c86a997e862226371ce66872
JheeKim <jheekim992@gmail.com>
2023-03-03 15:07:18 +0900

    nav active

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,8 @@
-// const toggleBtn = document.querySelector(".toggleBtn");
-// const menu = document.querySelector("#menu-list");
-// const topMenu = document.querySelector("#top-menu");
+const toggleBtn = document.querySelector(".toggleBtn");
+const menu = document.querySelector("#menu-list");
+const topMenu = document.querySelector("#top-menu");
 
-// toggleBtn.addEventListener("click", () => {
-//   menu.classList.toggle("active");
-//   topMenu.classList.toggle("active");
-// });
+toggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  topMenu.classList.toggle("active");
+});
