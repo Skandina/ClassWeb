76ffb3f7cb58ed58fb271b24e6fc7fdc958a7f1a
JheeKim <jheekim992@gmail.com>
2023-03-08 18:35:49 +0900

    Modified filter function, tab click event

--- js/board.js
+++ js/board.js
@@ -1,5 +1,4 @@
 `use strict`;
-const tbody = document.querySelector("tobody");
 
 const contents = [
   {
@@ -28,7 +27,7 @@ const contents = [
   },
   {
     level: "Beginner",
-    title: "Korean food recipe2",
+    title: "recipe4",
     date: "2023-06-15",
     image: "bibimbap.jpg",
     detail:
@@ -36,54 +35,114 @@ const contents = [
   },
 ];
 
-// 날짜 형식 변환해서 넣기
-// const currentMonth = new Date();
-// const months = [
-//   "January",
-//   "February",
-//   "March",
-//   "April",
-//   "May",
-//   "June",
-//   "July",
-//   "August",
-//   "September",
-//   "October",
-//   "November",
-//   "December",
-// ];
-// console.log(months[currentMonth.getMonth()]);
+const tabs = document.querySelectorAll("a.nav-link");
+const tabAll = document.querySelector(".tab-all");
+const tabBeginner = document.querySelector("a.tab-beginner");
+const tabIntermediate = document.querySelector(".tab-intermediate");
+const tabAdvanced = document.querySelector(".tab-advanced");
+const ACTIVE_CLASSNAME = "active";
+
 const docFragment = new DocumentFragment();
 const cardHtml = document.querySelector("#card-wrap");
 
-const filterBeginner = contents
-  .filter((x) => x.level === "Beginner")
-  .map((content) => {
-    const cardDiv = document.createElement("div");
-    cardDiv.classList.add("card", "mb-3");
-    const cardImg = document.createElement("img");
-    cardImg.classList.add("card-img-top");
-    cardImg.src = `img/${content.image}`;
-    cardDiv.appendChild(cardImg);
-
-    const cardBodyDiv = document.createElement("div");
-    cardBodyDiv.classList.add("card-body");
-    const cardTitle = document.createElement("h5");
-    cardTitle.classList.add("card-title");
-    cardTitle.textContent = content.title;
-    const cardText = document.createElement("p");
-    cardText.classList.add("card-text");
-    cardText.textContent = content.detail;
-    const cardDate = document.createElement("p");
-    cardDate.classList.add("card-text", "date");
-    cardDate.textContent = content.date;
-    // date format 필요
-
-    cardBodyDiv.appendChild(cardTitle);
-    cardBodyDiv.appendChild(cardText);
-    cardBodyDiv.appendChild(cardDate);
-
-    cardDiv.appendChild(cardBodyDiv);
-    docFragment.append(cardDiv);
+// 첫 화면이 All인 경우, contents 다 보여주기
+if (tabAll.classList.contains(ACTIVE_CLASSNAME)) {
+  contents.map((content) => {
+    createCard(content);
   });
-cardHtml.appendChild(docFragment);
+}
+
+// tab 클릭 시
+function handleTabClick(e) {
+  // 먼저 있던 active class 삭제하기
+  for (let tab of tabs) {
+    if (tab.classList.contains(ACTIVE_CLASSNAME)) {
+      tab.classList.remove(ACTIVE_CLASSNAME);
+    }
+  }
+
+  // 기존 데이터 삭제하기
+  const cardHtmlChilds = document.querySelectorAll("#card-wrap div");
+  if (cardHtml.hasChildNodes()) {
+    for (let child of cardHtmlChilds) {
+      child.remove();
+    }
+  }
+
+  // filtering 데이터 보여주기
+  const value = e.target.getAttribute("data-value");
+  if (value === "Beginner") {
+    contents
+      .filter((x) => x.level === "Beginner")
+      .map((content) => {
+        createCard(content);
+      });
+    tabBeginner.classList.toggle(ACTIVE_CLASSNAME);
+  } else if (value === "Intermediate") {
+    contents
+      .filter((x) => x.level === "Intermediate")
+      .map((content) => {
+        createCard(content);
+      });
+    tabIntermediate.classList.toggle(ACTIVE_CLASSNAME);
+  } else if (value === "Advanced") {
+    contents
+      .filter((x) => x.level === "Advanced")
+      .map((content) => {
+        createCard(content);
+      });
+    tabAdvanced.classList.toggle(ACTIVE_CLASSNAME);
+  } else if (value === "All") {
+    contents.map((content) => {
+      createCard(content);
+    });
+    tabAll.classList.toggle(ACTIVE_CLASSNAME);
+  }
+}
+
+function createCard(content) {
+  const cardDiv = document.createElement("div");
+  cardDiv.classList.add("card", "mb-3");
+  const cardImg = document.createElement("img");
+  cardImg.classList.add("card-img-top");
+  cardImg.src = `img/${content.image}`;
+  cardDiv.appendChild(cardImg);
+
+  const cardBodyDiv = document.createElement("div");
+  cardBodyDiv.classList.add("card-body");
+  const cardTitle = document.createElement("h5");
+  cardTitle.classList.add("card-title");
+  cardTitle.textContent = content.title;
+  const cardText = document.createElement("p");
+  cardText.classList.add("card-text");
+  cardText.textContent = content.detail;
+  let cardDate = document.createElement("p");
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  cardDate.classList.add("card-text", "date");
+  cardDate.textContent = new Date(content.date);
+  // date format 진행중
+  // console.log(cardDate.textContent);
+  // console.log(months[currentMonth.getMonth()]);
+
+  cardBodyDiv.appendChild(cardTitle);
+  cardBodyDiv.appendChild(cardText);
+  cardBodyDiv.appendChild(cardDate);
+
+  cardDiv.appendChild(cardBodyDiv);
+
+  docFragment.append(cardDiv);
+  cardHtml.appendChild(docFragment);
+}
